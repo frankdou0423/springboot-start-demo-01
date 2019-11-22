46ac633ce37bc6a13740281bd0499b1ff0d009f7
frankdou0423 <frankdou0423@126.com>
2019-11-22 09:21:31 +0800

    测试git commit

--- src/main/java/com/gtfrank/springboot/controller/IndexController.java
+++ src/main/java/com/gtfrank/springboot/controller/IndexController.java
@@ -1,5 +1,4 @@
 package com.gtfrank.springboot.controller;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,33 +6,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-/**
- * @ProjectName: springbootstartdemo01
- * @Package: com.gtfrank.springboot.controller
- * @ClassName: IndexController
- * @Author: frank
- * @Description: 测试springboot启动方式
- * @Date: 2019/11/20 11:11
- * @Version: 1.0
- */
 @RestController
 @SpringBootApplication
 public class IndexController {
     @RequestMapping(value = "/test",method = RequestMethod.GET)
     public String index(){
-        //测试
-        //测试2
-
-
+        //返回值可以是string也可以是其他的
         return "hello";
     }
-
+    /**
+     * @description:
+     * @param map
+     * @return: String
+     * @author: frank
+     * @time: 2019/11/21 15:26
+     */
     @RequestMapping(value = "/register",method = RequestMethod.POST)
     public String register(Map<String,Object> map){
         return "redirect:/text";
     }
-    public static void main(String[] args) {
-        //这里是springboot的入口函数
-        SpringApplication.run(IndexController.class,args);
-    }
+
 }
